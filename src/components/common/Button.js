b0771601224d207424ea77bbaae9fc718d5c7ad9
irom77 <irek.romaniuk@gmail.com>
2017-06-23 08:31:05 -0400

    copied from auth.good

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -17,10 +17,12 @@ const styles = {
         alignSelf: 'center',
         color: '#007aff',
         fontSize: 16,
-        fontWeight: '600'
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
     },
     buttonStyle: {
-        flex: 1,
+        flexGrow: 1,
         alignSelf: 'stretch',
         backgroundColor: '#fff',
         borderRadius: 5,
